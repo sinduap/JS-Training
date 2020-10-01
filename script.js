7d2b2bef6ee5afc6a89662b4b4abf7c3554f430d
Sindu Andita <sinduandita@gmail.com>
2020-10-01 17:02:27 +0700

    Updates

--- script.js
+++ script.js
@@ -206,4 +206,40 @@ do {
   i++;
 } while (i < 5);
 
-//
+//Profile Lookup
+
+var contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Cases", "Violine", "Smoke"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", " Foxes"],
+  },
+];
+
+function lookUp(name, prop) {
+  for (i = 0; i < contacts.length; i++) {
+    if (name === contacts[i].firstName) {
+      return contacts[i][prop] || "Prop false";
+    }
+  }
+  return "Not Found";
+}
