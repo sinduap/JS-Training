c5ebc87fbe8f0634658c98156b72c4e7a854c883
Sindu Andita <sinduandita@gmail.com>
2020-10-02 13:51:08 +0700

    Updates

--- script.js
+++ script.js
@@ -311,10 +311,24 @@ var anonFunc = function () {
 
 var anonFunc = () => new Date();
 
-/////example 2
+/////example 2 arrow function with parameters
 
 var myConcat = function (arr1, arr2) {
   return arr1.concat(arr2);
 };
 
 var myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+//
+
+const realNumberArray = [4, 5.6, -9.6, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+  const squaredIntegers = arr
+    .filter((num) => Number.isInteger(num) && num > 0)
+    .map((x) => x * x);
+  return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
